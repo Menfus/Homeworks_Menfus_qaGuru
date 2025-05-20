e5f10640c4894c1723b6bc1b575e59e9de0afeff
Menfus <Menfus369@gmail.com>
2025-05-20 19:08:42 +0300

    Update TestBase.java

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,26 +1,40 @@
-package tests;
+package qaguru.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import qaguru.utils.Attach;
 
 import java.util.Map;
-
-import static com.codeborne.selenide.Configuration.baseUrl;
-import static com.codeborne.selenide.Selenide.executeJavaScript;
 
 public class TestBase {
 
     @BeforeAll
-    static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = "normal";
-        // Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 10000;
+    static void setUp() {
 
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        String selenoidHost = System.getProperty("SELENOID_HOST", "selenoid.autotests.cloud");
+        String browser = System.getProperty("browser", "chrome");
+        String browserVersion = System.getProperty("browserVersion", "127.0");
+        String screenResolution = System.getProperty("screenResolution", "1920x1080");
+
+        WebDriverManager.chromedriver()
+                .clearDriverCache()
+                .clearResolutionCache()
+                .setup();
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = screenResolution;
+        Configuration.browser = browser;
+        Configuration.browserVersion = browserVersion;
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 10000;
+        Configuration.remote = String.format("https://user1:1234@%s/wd/hub",
+                selenoidHost);
+
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
@@ -30,9 +44,14 @@ public class TestBase {
         Configuration.browserCapabilities = capabilities;
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
-
     }
 
-}
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
 
+    }
+}
