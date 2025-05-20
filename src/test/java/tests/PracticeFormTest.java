ebb32fb0e15d30716d83204ab7cc0db9ce487f66
Menfus <Menfus369@mail.ru>
2025-05-20 18:52:46 +0300

    Fix

--- src/test/java/tests/PracticeFormTest.java
+++ src/test/java/tests/PracticeFormTest.java
@@ -16,7 +16,7 @@ import static com.codeborne.selenide.Selectors.by;
 import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
-@Tag("simple")
+@Tag("simplee")
     public class PracticeFormTest extends TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
 
@@ -92,6 +92,7 @@ import static io.qameta.allure.Allure.step;
                     .checkResult("Gender", gender);
         });
     }
+}
 
 
 
