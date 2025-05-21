bb6a54ab720b372f214188d788171e8e00ffcd76
Menfus <Menfus369@gmail.com>
2025-05-21 13:48:29 +0300

    Update PracticeFormTest.java

--- src/test/java/tests/PracticeFormTest.java
+++ src/test/java/tests/PracticeFormTest.java
@@ -16,11 +16,35 @@ import static com.codeborne.selenide.Selectors.by;
 import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
+@Tag("simple")
+    public class PracticeFormTest extends TestBase {
+    RegistrationPage registrationPage = new RegistrationPage();
 
-RegistrationPage practiceFormPage = new RegistrationPage();
-SubmittingFormComponent submittingFormComponent = new SubmittingFormComponent();
+    //Создаем рандомные данные через библиотеку Faker
+    Faker faker = new Faker();
 
-    @Tag("simple")
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
+    //String gender = faker.options().option("Male", "Female", "Other");
+    String gender = "Male";
+    String userEmail = faker.internet().emailAddress();
+    String userNumber = faker.number().digits(10);
+    String birthDay = String.format("%02d", faker.number().numberBetween(1, 28));
+    String birthMonth = faker.options().option(
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December");
+    String birthYear = String.valueOf(faker.number().numberBetween(1950, 2000));
+
+    String subject = faker.options().option(
+            "Chemistry", "Maths", "Physics", "Arts", "English",
+            "Biology", "History", "Economics", "Computer Science", "Commerce", "Accounting");
+    String hobby = "Sport";
+    //String hobby = faker.options().option("Sport", "Reading", "Music");
+    String streetAddress = faker.address().streetAddress();
+    String state = "Haryana";
+    String city = "Karnal";
+
+
     @Feature("Форма регистрации")
     @Story("Успешная отправка формы")
     @Owner("Menfus")
@@ -31,34 +55,36 @@ SubmittingFormComponent submittingFormComponent = new SubmittingFormComponent();
 
         step("Заполняем поля регистрационной формы", () -> {
 
-           $("#firstName").setValue("Alex");
-            $("#lastName").setValue("Egorov");
-            $("#userEmail").setValue("alex@egorov.com");
-            $("#genterWrapper").$(byText("Other")).click();
-            $("#userNumber").setValue("1234567890");
-            $("#dateOfBirthInput").click();
-            $(".react-datepicker__month-select").selectOption("July");
-            $(".react-datepicker__year-select").selectOption("2008");
-            $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
-            $("#subjectsInput").setValue("Math").pressEnter();
-            $("#hobbiesWrapper").$(byText("Sports")).click();
-            $("#uploadPicture").uploadFromClasspath("img/1.png");
-            $("#currentAddress").setValue("Some address 1");
-            $("#state").click();
-            $("#stateCity-wrapper").$(byText("NCR")).click();
-            $("#city").click();
-            $("#stateCity-wrapper").$(byText("Delhi")).click();
-            $("#submit").click();
+            registrationPage.openPage()
+                    .setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setEmail(userEmail)
+                    .setUserNumber(userNumber)
+                    .setGender(gender)
+                    .setDateOfBirth(birthDay, birthMonth, birthYear)
+                    .setSubjects(subject)
+                    .setHobby(hobby)
+                    .setPicture("testIMG.png")
+                    .setAdress(streetAddress)
+                    .setState(state)
+                    .setCity(city)
+                    .getSubmit();
         });
         step("Проверяем отправленные значения в полях регистрационной формы", () -> {
 
-             $(".modal-dialog").should(appear);
-            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-            $(".table-responsive").shouldHave(text("Alex"), text("Egorov"),
-                    text("alex@egorov.com"), text("1234567890"));
+            registrationPage
+                    .checkResult("Student Name", firstName + " " + lastName)
+                    .checkResult("Student Email", userEmail)
+                    .checkResult("Mobile", userNumber)
+                    .checkResult("Date of Birth", birthDay + " " + birthMonth + "," + birthYear)
+                    .checkResult("Subjects", subject)
+                    .checkResult("Picture", "testIMG.png")
+                    .checkResult("Address", streetAddress)
+                    .checkResult("State and City", state + " " + city)
+                    .checkResult("Hobbies", hobby)
+                    .checkResult("Gender", gender);
         });
     }
-
 }
 
 
