b1c9951441d501410b2890a430d9e411f16e0665
Menfus <Menfus369@mail.ru>
2025-05-22 17:16:45 +0300

    zxx

--- src/test/java/properties/SystemPropertiesTests.java
+++ src/test/java/properties/SystemPropertiesTests.java
@@ -1,21 +1,22 @@
 package properties;
 
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
-
-import static java.lang.String.format;
-public class SystemPropertiesTests {
-
-    @Test
-    @Tag("property")
-    void systemProperties4Test() {
-        System.out.println(browser);
-        // gradle property_test
-        // mozilla
-
-        // gradle property_test -Dbrowser=opera
-        // opera
-
-    }
-}
+//import org.junit.jupiter.api.Tag;
+//import org.junit.jupiter.api.Test;
+//
+//import static com.codeborne.selenide.Configuration.browser;
+//import static java.lang.String.format;
+//public class SystemPropertiesTests {
+//
+//    @Test
+//    @Tag("simple")
+//    void systemProperties4Test() {
+//        System.out.println(browser);
+//        // gradle property_test
+//        // mozilla
+//
+//        // gradle property_test -Dbrowser=opera
+//        // opera
+//
+//    }
+//}
 
